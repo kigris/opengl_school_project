93b1070f5cb384f7c88e709ef2ed0ec16f4b52fe
Kigris <kigrislord@gmail.com>
2023-05-25 11:57:25 +0200

    Exercise 03 - Create a flashlight, check out Project "12_LightSpot"

--- src/engine/camera.cpp
+++ src/engine/camera.cpp
@@ -1,6 +1,7 @@
 #include "engine/camera.hpp"
 
-Camera::Camera(const glm::vec3& position, const glm::vec3& up, float yaw, float pitch) {
+Camera::Camera(const glm::vec3 &position, const glm::vec3 &up, float yaw, float pitch)
+{
     position_ = position;
     worldUp_ = up;
     yaw_ = yaw;
@@ -9,7 +10,8 @@ Camera::Camera(const glm::vec3& position, const glm::vec3& up, float yaw, float 
     updateCameraVectors();
 }
 
-Camera::Camera(float posX, float posY, float posZ, float upX, float upY, float upZ, float yaw, float pitch) {
+Camera::Camera(float posX, float posY, float posZ, float upX, float upY, float upZ, float yaw, float pitch)
+{
     position_ = glm::vec3(posX, posY, posZ);
     worldUp_ = glm::vec3(upX, upY, upZ);
     yaw_ = yaw;
@@ -18,19 +20,38 @@ Camera::Camera(float posX, float posY, float posZ, float upX, float upY, float u
     updateCameraVectors();
 }
 
-glm::mat4 Camera::getViewMatrix() const {
+glm::mat4 Camera::getViewMatrix() const
+{
     return glm::lookAt(position_, position_ + front_, up_);
 }
 
-float Camera::getFOV()  const {
+float Camera::getFOV() const
+{
     return fov_;
 }
 
-glm::vec3 Camera::getPosition() const {
+glm::vec3 Camera::getPosition() const
+{
     return position_;
 }
 
-void Camera::updateCameraVectors() {
+glm::vec3 Camera::getDirection() const
+{
+    // We could just return the front vector, which is the direction
+    return front_;
+    // // Or we could extract the direction from the lookAt matrix
+    // // We need to invert it to be in world space, not in camera space
+    // glm::mat4 invertedLookAtMatrix = glm::inverse(getViewMatrix());
+    // // Get the 3rd component, which is the direction vector
+    // glm::vec3 frontDirection = glm::vec3(invertedLookAtMatrix[2]);
+
+    // // Normalize and negate it so it is facing forward and its length is 1
+    // frontDirection = glm::normalize(-frontDirection);
+    // return frontDirection;
+}
+
+void Camera::updateCameraVectors()
+{
     glm::vec3 front;
     front.x = cos(glm::radians(yaw_)) * cos(glm::radians(pitch_));
     front.y = sin(glm::radians(pitch_));
@@ -41,33 +62,51 @@ void Camera::updateCameraVectors() {
     up_ = glm::normalize(glm::cross(right_, front_));
 }
 
-void Camera::handleKeyboard(Movement direction, float dt) {
+void Camera::handleKeyboard(Movement direction, float dt)
+{
     const float velocity = k_Speed * dt;
-    switch (direction) {
-        case Movement::Forward:   position_ += front_ * velocity; break;
-        case Movement::Backward:  position_ -= front_ * velocity; break;
-        case Movement::Left:      position_ -= right_ * velocity; break;
-        case Movement::Right:     position_ += right_ * velocity; break;
+    switch (direction)
+    {
+    case Movement::Forward:
+        position_ += front_ * velocity;
+        break;
+    case Movement::Backward:
+        position_ -= front_ * velocity;
+        break;
+    case Movement::Left:
+        position_ -= right_ * velocity;
+        break;
+    case Movement::Right:
+        position_ += right_ * velocity;
+        break;
     }
 }
 
-void Camera::handleMouseMovement(float xoffset, float yoffset, bool constrainPitch) {
+void Camera::handleMouseMovement(float xoffset, float yoffset, bool constrainPitch)
+{
     const float xoff = xoffset * k_Sensitivity;
     const float yoff = yoffset * k_Sensitivity;
 
     yaw_ += xoff;
     pitch_ += yoff;
 
-    if (constrainPitch) {
-        if (pitch_ > 89.0f) pitch_ = 89.0f;
-        if (pitch_ < -89.0f) pitch_ = -89.0f;
+    if (constrainPitch)
+    {
+        if (pitch_ > 89.0f)
+            pitch_ = 89.0f;
+        if (pitch_ < -89.0f)
+            pitch_ = -89.0f;
     }
 
     updateCameraVectors();
 }
 
-void Camera::handleMouseScroll(float yoffset) {
-    if (fov_ >= 1.0f && fov_ <= 45.0f) fov_ -= yoffset;
-    if (fov_ <= 1.0f) fov_ = 1.0f;
-    if (fov_ >= 45.0f) fov_ = 45.0f;
+void Camera::handleMouseScroll(float yoffset)
+{
+    if (fov_ >= 1.0f && fov_ <= 45.0f)
+        fov_ -= yoffset;
+    if (fov_ <= 1.0f)
+        fov_ = 1.0f;
+    if (fov_ >= 45.0f)
+        fov_ = 45.0f;
 }
